ed1c89cbcc67650cdb38668bdb10e8b91c6020b2
rzvn666 <ionut-razvan.cristea@warwick.ac.uk>
2023-02-26 13:00:36 +0000

    changed db collation for compatability with laptop

--- accounts/accounts.sql
+++ accounts/accounts.sql
@@ -32,10 +32,10 @@ CREATE TABLE `accounts` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `pass` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `pass` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_created` date NOT NULL,
   `admin` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `accounts`
