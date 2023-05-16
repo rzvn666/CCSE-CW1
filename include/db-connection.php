a4d64e01b17f85a4e264182f50d431d5bf267f03
Razvan Cristea <61581943+rzvn666@users.noreply.github.com>
2023-05-16 20:26:27 +0100

    Update db-connection.php

--- include/db-connection.php
+++ include/db-connection.php
@@ -1,13 +1,19 @@
- <?php
-    $servername = "127.0.0.1";
-    $username = "ccse";
-    $password = "ccse";
+<?php
+$host = 'ccse-cw2-razvan-server.mysql.database.azure.com';
+$username = 'izbltcobyp';
+$password = '3RU2TODC52C70L03$';
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
+//Initializes MySQLi
+$conn = mysqli_init();
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-?> 
+mysqli_ssl_set($conn,NULL,NULL, "/home/site/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, NULL, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+?>
